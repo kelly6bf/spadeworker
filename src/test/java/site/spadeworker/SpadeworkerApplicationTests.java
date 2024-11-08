4e9ffe989217bcb053abe45efa64f3d08caae1e9
kelly6bf <kelly6bf@gmail.com>
2024-11-08 17:48:10 +0900

    init project

---
+++ src/test/java/site/spadeworker/SpadeworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.spadeworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpadeworkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
